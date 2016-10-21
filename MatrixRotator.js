930fe3f73bcff7caa03de27a0155899a4c0e34e6
Raymond Luong <raymond.n.luong@gmail.com>
2016-10-20 18:54:19 -1000

    Code cleanup

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -1,7 +1,7 @@
 /*  MatrixRotator(matrix)
  *
  *  @param matrix                        a multidimensional array containing the matrix
- *  
+ *
  *  @public property matrix              the matrix
  *
  *  @public method rotate(direction)     direction is either
@@ -13,36 +13,30 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
 };
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
 MatrixRotator.prototype.rotate = function(direction) {
-  // do work here
   let newMatrix = [];
   const matrixLen = this.matrix.length;
 
   if (direction === Direction.CW) {
-  	for (let i = 0; i < matrixLen; i++) {
-  		newMatrix[i] = [];
-  		for (let j = 0; j < matrixLen; j++) {
-  			newMatrix[i][j] = this.matrix[matrixLen - 1 - j][i];
-  		}
-  	}
+    for (let i = 0; i < matrixLen; i++) {
+      newMatrix[i] = [];
+      for (let j = 0; j < matrixLen; j++) {
+        newMatrix[i][j] = this.matrix[matrixLen - 1 - j][i];
+      }
+    }
   }
 
   if (direction === Direction.CCW) {
-  	for (let i = 0; i < matrixLen; i++) {
-  		newMatrix[i] = [];
-  		for (let j = 0; j < matrixLen; j++) {
-  			newMatrix[i][j] = this.matrix[j][matrixLen - 1 - i];
-  		}
-  	}
+    for (let i = 0; i < matrixLen; i++) {
+      newMatrix[i] = [];
+      for (let j = 0; j < matrixLen; j++) {
+        newMatrix[i][j] = this.matrix[j][matrixLen - 1 - i];
+      }
+    }
   }
-
   this.matrix = newMatrix;
-  // must be a valid Direction, see Direction.js
-
-
 };
